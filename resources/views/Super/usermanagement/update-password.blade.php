59edf3a879fd741b9ec4e3bcdd97818d0527707f
Rex <rexlesterbastaoang0811@gmail.com>
2024-10-01 18:20:30 +0800

    plan

--- resources/views/Super/usermanagement/update-password.blade.php
+++ resources/views/Super/usermanagement/update-password.blade.php
@@ -12,7 +12,7 @@
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>User Activity Log</h3>
+                    <h3>User Activity Logs</h3>
                     <p class="text-subtitle text-muted">For user log list</p>
                 </div>
                 <div class="col-12 col-md-6 order-md-2 order-first">
