ec483314a8c10db1ba4c4a42b311971f453db132
Rex <rexlesterbastaoang0811@gmail.com>
2024-10-22 15:51:40 +0800

    todaychangess

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
     <td class="header" style="text-align: center; padding: 20px;">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'Laravel')
-            <img src="{{ asset('imagezz/logo.jpg') }}" width="70" height="70" alt="Logo" style="display: block;">
+            <img src="{{ asset('imagezz/email_logo.jpg') }}" width="70" height="70" alt="Logo" style="display: block;">
             @else
             <span style="font-size: 20px; font-weight: bold; color: #333;">{{ $slot }}</span>
             @endif
