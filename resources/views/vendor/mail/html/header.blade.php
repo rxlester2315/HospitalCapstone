443cea7ab92974dea9d7d5431ac5464687f1797b
Rex <rexlesterbastaoang0811@gmail.com>
2024-10-29 18:43:03 +0800

    fixed emailS

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,7 @@
         <a href="{{ $url }}" style="display: inline-block; text-decoration: none; max-width: 100%;">
             @if (trim($slot) === 'Laravel')
             <!-- Centered Logo with modern styling and alignment -->
-            <img src="{{ asset('imagezz/email_logo.jpg') }}" width="80" height="80" alt="Logo"
+            <img src="{{asset('admins/assets/images/logo/logo.png')}}" width="80" height="80" alt="Logo"
                 style="display: block; margin: 0 auto 10px; border-radius: 10px; max-width: 100%;">
             @else
             <!-- Custom text with enhanced readability and spacing -->
