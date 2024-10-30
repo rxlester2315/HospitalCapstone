3ed50e5f02d29cee6a562e524240e041271f91cc
Rex <rexlesterbastaoang0811@gmail.com>
2024-10-30 09:24:25 +0800

    last fixed

--- resources/views/Super/usermanagement/update-password.blade.php
+++ resources/views/Super/usermanagement/update-password.blade.php
@@ -12,7 +12,7 @@
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>User Activity Logs</h3>
+                    <h3> Reset Password</h3>
                     <p class="text-subtitle text-muted">Account Reset Password</p>
                 </div>
                 <div class="col-12 col-md-6 order-md-2 order-first">
